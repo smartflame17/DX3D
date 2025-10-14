ce1733f97d94d3b04d3eb5447b4457ebf7de7eda
Jason Kim <jasonjiseop.kim@gmail.com>
2025-10-14 22:42:27 +0900

    fix : fixed multiplication order

--- Shaders/VertexShader.hlsl
+++ Shaders/VertexShader.hlsl
@@ -12,7 +12,7 @@ cbuffer cBuf
 VSOut main( float2 pos : POSITION, float3 color : COLOR )
 {
     VSOut vso;
-    vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);
+    vso.pos = mul(transform, float4(pos.x, pos.y, 0.0f, 1.0f));
     vso.color = color;
     return vso;
 }
